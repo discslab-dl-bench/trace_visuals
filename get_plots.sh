9ec35c51fe959b252d814fb805d120f1f03e9dd4
RuoyuDeng <rockdenggg@gmail.com>
2022-08-18 17:24:18 -0400

    Simple script to preprocess and plot traces

---
+++ get_plots.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+cd data
+traces_dir=$1
+ta_traces_dir="ta_${traces_dir}"
+num_gpus=$2
+exp_name=$3
+py=python3
+
+
+if [ $# -lt 3 ]
+then
+    # example: ./get_plots.sh exp_4gpus_20220818150543 4 4gpu
+	echo "Usage: $0 <traces_dir> <numgpus> <experiment_name>"
+	exit 1
+fi
+
+
+preprocessing
+./preprocess_traces.sh $traces_dir $num_gpus
+
+cd ..
+# install the missing modules in the python used in bash (uncomment the next 2 lines if you are running the script for the first time)
+# pip install matplotlib
+# pip install IPython
+${py} timeline.py data/$ta_traces_dir $exp_name
