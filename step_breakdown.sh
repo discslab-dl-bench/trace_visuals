59d65eacd0b3d5ac6d763203b0a7c8950dcef489
jovonho <loic.hovon@gmail.com>
2023-05-05 09:13:51 -0400

    cleanup

--- step_breakdown.sh
+++
@@ -1,8 +0,0 @@
-# Little script to move the processed app logs to a folder for step breakdwon processing
-mkdir -p data_step_breakdown/DLRM_instru_4/raw_data
-
-for d in $( ls data/mar16/DLRM_instru_4 )
-do
-    echo $d
-    cp data/mar16/DLRM_instru_4/${d}/processed/dlrm.log data_step_breakdown/DLRM_instru_4/raw_data/${d}.json
-done
